73104a6f10132e319bf15f30b71e6f4ca8f68f17
unknown <insomniq4@abv.bg>
2025-08-30 16:46:01 +0600

    Create interface for service and add create service logic in TaskServiceImpl class

--- spring-todo-api/src/main/java/com/example/spring_todo_api/service/TaskService.java
+++ spring-todo-api/src/main/java/com/example/spring_todo_api/service/TaskService.java
@@ -1,23 +1,10 @@
 package com.example.spring_todo_api.service;
 
 import com.example.spring_todo_api.model.Task;
-import com.example.spring_todo_api.repository.TaskRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class TaskService {
-
-    private final TaskRepository repo;
-
-    @Autowired
-    TaskService(TaskRepository repo){
-        this.repo = repo;
-    }
-
-    public List<Task> getAllTasks(){
-        return repo.findAll();
-    }
+interface TaskService{
+    List<Task> getAllTasks();
+    Task createTask(Task task);
 }
---
+++ spring-todo-api/src/main/java/com/example/spring_todo_api/service/TaskServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.spring_todo_api.service;
+
+import com.example.spring_todo_api.model.Task;
+import com.example.spring_todo_api.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskServiceImpl implements TaskService{
+
+    private final TaskRepository repo;
+
+    @Autowired
+    TaskServiceImpl(TaskRepository repo){
+        this.repo = repo;
+    }
+
+    public List<Task> getAllTasks(){
+        return repo.findAll();
+    }
+
+    public Task createTask(Task task){
+        repo.save(task);
+        return task;
+    }
+}

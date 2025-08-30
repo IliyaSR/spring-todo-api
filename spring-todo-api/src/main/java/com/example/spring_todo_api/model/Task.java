f94a2dbd9eaf29c437b50d758b0e7024a3288fb1
unknown <insomniq4@abv.bg>
2025-08-30 15:04:18 +0600

    Add validation on Task class

--- spring-todo-api/src/main/java/com/example/spring_todo_api/model/Task.java
+++ spring-todo-api/src/main/java/com/example/spring_todo_api/model/Task.java
@@ -1,25 +1,41 @@
 package com.example.spring_todo_api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Table(name = "tasks")
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "Title is required!")
+    @Size(min = 2, max = 50, message = "Title must be between 2 and 50 letters!")
+    @Column(nullable = false)
     private String title;
+
     private String description;
-    private boolean completed;
+
+    @NotNull(message = "Completed field is required!")
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    @NotNull(message = "CreatedAt is required!")
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
